60acb713d469367558b5587db81c4bd99a3abae0
jpacsai <judit.pacsai@gmail.com>
2018-02-26 19:41:56 +0100

    adding js snippet

--- JavaScript_snippets/RemoveDuplicate.js
+++ JavaScript_snippets/RemoveDuplicate.js
@@ -14,3 +14,23 @@ function removeDuplicate(arr) {
 
 
   removeDuplicate([2, 3, 3, 1, 5, 2]);  // [2, 3, 1, 5]
+
+
+// REMOVE BOTH MULTIPLES //
+
+var myArr = [1, 1, 2, 5, 5, 7, 8, 9, 9];
+
+function removeDuplicates(arr) {
+    var i, tmp;
+    for(i=0; i<arr.length; i++) {
+        tmp = arr.lastIndexOf(arr[i]);
+        if(tmp === i) {
+            //Only one of this number
+        } else {
+            //More than one
+            arr.splice(tmp, 1);
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
